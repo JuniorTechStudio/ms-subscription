389cf20c69a08106c124351aad56f69cf9ac7b90
pavlikmeow <titanovpoo@gmail.com>
2022-11-04 18:22:02 +0400

    feature/0.1 много всего)

--- src/main/java/com/jts/subscription/subscription/client/ContentClient.java
+++ src/main/java/com/jts/subscription/subscription/client/ContentClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "ms-content", url = "http://localhost:8084/subscription")
+@FeignClient(name = "ms-content", url = "http://192.168.88.13:8084/subscription")
 public interface ContentClient {
 
     @RequestMapping(method = RequestMethod.POST)
