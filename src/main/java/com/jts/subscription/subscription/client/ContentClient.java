7faf782b7be10c63a311b3107bd24d794f9908ea
Bichik Artem <a.b.220@yandex.ru>
2022-12-04 20:07:17 +0300

    develop path for ContentClient was changed

--- src/main/java/com/jts/subscription/subscription/client/ContentClient.java
+++ src/main/java/com/jts/subscription/subscription/client/ContentClient.java
@@ -5,11 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//TODO
-@FeignClient(name = "ms-content", url = "localhost:8084/subscription")
+@FeignClient(name = "ms-content", url = "localhost:8084/")
 public interface ContentClient {
 
-    @PostMapping
-    void prepareAndSendContent(@RequestBody PrepareAndSendContentRequest prepareAndSendContentRequest);
+    @PostMapping("subscription/send")
+    void prepareAndSendContent(@RequestBody PrepareAndSendContentRequest request);
 
 }
